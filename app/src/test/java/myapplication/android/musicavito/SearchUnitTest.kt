904e7f30d90a018abe7c15a3a7bf44ef462f7359
Shadow Berserker <iuliaglad07@gmail.com>
2025-04-21 21:25:57 +0300

    Search unit test added

--- app/src/test/java/myapplication/android/musicavito/SearchUnitTest.kt
+++ app/src/test/java/myapplication/android/musicavito/SearchUnitTest.kt
@@ -12,11 +12,6 @@ import org.junit.Test
 
 import org.junit.Assert.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class SearchUnitTest {
     private val downloadTracksReducer: DownloadedTracksReducer = DownloadedTracksReducer()
     private val trackTwo = TrackUi(
